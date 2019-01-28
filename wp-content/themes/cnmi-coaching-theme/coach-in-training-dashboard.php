6aa5a4ca44fb5bc7865d169abdf4c4b54969c0a6
Eric Debelak <ericdebelak@gmail.com>
2019-01-28 12:23:36 -0700

    ADDED: start of dashboard design

--- wp-content/themes/cnmi-coaching-theme/coach-in-training-dashboard.php
+++ wp-content/themes/cnmi-coaching-theme/coach-in-training-dashboard.php
@@ -2,3 +2,31 @@
 ?>
 
 <h2>You are a Coach in Training!</h2>
+
+<div class='dashboard-block-container'>
+	
+	<a href='/my-certifications'>
+		<div class='one-half first my-certifications'>
+			<h3>My Certifications</h3>
+		</div>
+	</a>
+
+	<a href='/my-certifications'>
+		<div class='one-half my-certifications'>
+			<h3>My Certifications</h3>
+		</div>
+	</a>
+
+	<a href='/my-certifications'>
+		<div class='one-half first my-certifications'>
+			<h3>My Certifications</h3>
+		</div>
+	</a>
+
+	<a href='/my-certifications'>
+		<div class='one-half my-certifications'>
+			<h3>My Certifications</h3>
+		</div>
+	</a>
+
+</div>
