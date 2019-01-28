c061e1c00ed8f9443f1777d51ca7b91fa4e94a45
Eric Debelak <ericdebelak@gmail.com>
2019-01-28 10:49:21 -0700

    ADDED: form validation to coaching hours

--- wp-content/themes/cnmi-coaching-theme/partials/coaching-hours-form.php
+++ wp-content/themes/cnmi-coaching-theme/partials/coaching-hours-form.php
@@ -22,22 +22,25 @@ if (
 }
 ?>
 <h1>Submit Coaching Hours</h1>
-<form action="" method="POST" enctype="multipart/form-data">
+<form id="coaching-hours-form" action="" method="POST" enctype="multipart/form-data">
 	<label for="id">Progress ID</label>
-	<input label="ID" name="id" type="number">
+	<input label="ID" name="id" type="number" required>
 
 	<label for="client_name">Client Name</label>
-	<input label="client_name" name="client_name">
+	<input label="client_name" name="client_name" required>
 
 	<label for="date">Date</label>
-	<input label="date" name="date" type="date">
+	<input label="date" name="date" type="date" required>
 
 	<label for="minutes">Minutes</label>
-	<input label="minutes" name="minutes" type="number">
+	<input label="minutes" name="minutes" type="number" required>
 
 	<label for="comments">Comments</label>
-	<textarea label="comments" name="comments"></textarea>
+	<textarea label="comments" name="comments" required></textarea>
 	
 	<input type="submit" value="Submit" name="submit">
 	<?php wp_nonce_field( 'coaching_hours', 'coaching_hours' ); ?>
 </form>
+<script>
+	jQuery("#coaching-hours-form").validate();
+</script>
