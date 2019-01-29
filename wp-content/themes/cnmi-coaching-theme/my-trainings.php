faa713a00ced06515150f460d185fb5f2c3f1810
Eric Debelak <ericdebelak@gmail.com>
2019-01-29 11:58:24 -0700

    ADDED: my training templates

--- wp-content/themes/cnmi-coaching-theme/my-trainings.php
+++ wp-content/themes/cnmi-coaching-theme/my-trainings.php
@@ -1,14 +1,14 @@
 <?php
 /*
- * Template Name: My Certifications
+ * Template Name: My Trainings
  */
 remove_action( 'genesis_entry_header', 'genesis_do_post_title' );
 
-function show_my_certifications() {
+function show_my_trainings() {
 	get_template_part('partials/elements/top-matter');	
-	$linkAddress = '/my-certification?certification=';
+	$linkAddress = '/my-training?training=';
 	include(locate_template('partials/elements/certification-list.php'));	
 }
-add_action('genesis_entry_content', 'show_my_certifications');
+add_action('genesis_entry_content', 'show_my_trainings');
 
 genesis();
