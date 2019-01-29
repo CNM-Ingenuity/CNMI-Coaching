a458cf99c3466c26a7ab8362782435bb80f68778
Eric Debelak <ericdebelak@gmail.com>
2019-01-29 15:32:42 -0700

    ADDED: breadcrumbs everywhere

--- wp-content/themes/cnmi-coaching-theme/my-resources.php
+++ wp-content/themes/cnmi-coaching-theme/my-resources.php
@@ -5,6 +5,10 @@
 remove_action( 'genesis_entry_header', 'genesis_do_post_title' );
 
 function show_my_resources() {
+	$breadcrumbs = [
+		"Resources" => "/my-resources"
+	];
+	include(locate_template('partials/elements/breadcrumbs.php'));	
 	get_template_part('partials/elements/top-matter');	
 	$linkAddress = '/my-resource?resource=';
 	include(locate_template('partials/elements/certification-list.php'));	
--- wp-content/themes/cnmi-coaching-theme/my-trainings.php
+++ wp-content/themes/cnmi-coaching-theme/my-trainings.php
@@ -5,6 +5,10 @@
 remove_action( 'genesis_entry_header', 'genesis_do_post_title' );
 
 function show_my_trainings() {
+	$breadcrumbs = [
+		"My Trainings" => "/my-trainings"
+	];
+	include(locate_template('partials/elements/breadcrumbs.php'));	
 	get_template_part('partials/elements/top-matter');	
 	$linkAddress = '/my-training?training=';
 	include(locate_template('partials/elements/certification-list.php'));	
