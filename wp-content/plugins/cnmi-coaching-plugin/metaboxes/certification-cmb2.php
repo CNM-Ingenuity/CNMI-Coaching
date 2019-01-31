443ef8b80af095a97923ec45f00a4e22193e2ba9
Eric Debelak <ericdebelak@gmail.com>
2019-01-31 15:50:55 -0700

    ADDED: classes for new tables and removing unneeded fields on cmb2

--- wp-content/plugins/cnmi-coaching-plugin/metaboxes/certification-cmb2.php
+++ wp-content/plugins/cnmi-coaching-plugin/metaboxes/certification-cmb2.php
@@ -26,35 +26,11 @@ if(!function_exists('cnmi_register_certification_metabox')) {
       'id' => $prefix . 'training_type',
       'type' => 'text',
     ));
-    $cmb->add_field( array(
-      'name' => __('Transcripts', 'cmb2'),
-      'desc' => __('Transcript of instruction', 'cmb2'),
-      'id' => $prefix . 'transcript',
-      'type' => 'text_url',
-    ));
     //Not sure if this should be a file or a URL
     $cmb->add_field( array(
       'name' => __('Assessment', 'cmb2'),
       'desc' => __('Assessment', 'cmb2'),
       'id' => $prefix . 'assessment',
-      'type' => 'text_url',
-    ));
-    $cmb->add_field( array(
-      'name' => __('Submit Coaching Session', 'cmb2'),
-      'desc' => __('Link to view to submit coaching session', 'cmb2'),
-      'id' => $prefix . 'submit_coaching_session',
-      'type' => 'text_url',
-    ));
-    $cmb->add_field( array(
-      'name' => __('Submit Letters of Reference', 'cmb2'),
-      'desc' => __('Link to view to submit letters of reference', 'cmb2'),
-      'id' => $prefix . 'letters_of_reference',
-      'type' => 'text_url',
-    ));
-    $cmb->add_field( array(
-      'name' => __('Coach End User Agreement', 'cmb2'),
-      'desc' => __('Link to Coach End User Agreement', 'cmb2'),
-      'id' => $prefix . 'coach_end_user_agreement',
       'type' => 'text_url',
     ));
     $cmb->add_field( array(
@@ -79,16 +55,6 @@ if(!function_exists('cnmi_register_certification_metabox')) {
         'id'               => 'file',
         'type'             => 'file'
     ) );
-    $cmb->add_field( array(
-      'name' => __('Training', 'cmb2'),
-      'desc' => __('Link to Training', 'cmb2'),
-      'id' => $prefix . 'training',
-      'type' => 'text_url',
-      'repeatable' => true,
-    	'text' => array(
-        'add_row_text' => 'Add Training',
-      ),
-    ));
   }
 }
 
--- wp-content/plugins/cnmi-coaching-plugin/metaboxes/event-cmb2.php
+++ wp-content/plugins/cnmi-coaching-plugin/metaboxes/event-cmb2.php
@@ -20,13 +20,6 @@ if(!function_exists('cnmi_register_event_metabox')) {
         'options' => cmb2_get_user_options( array( 'fields' => array( 'user_login' ) ) ),
     ));
     $cmb->add_field(array(
-        'name'    => __( 'Requirements', 'cmb2' ),
-        'desc'    => __( 'Certification Requirements', 'cmb2' ),
-        'id'      => $prefix . 'requirements',
-        'type'    => 'text',
-        'repeatable' => 'true'
-    ));
-    $cmb->add_field(array(
         'name'    => __( 'Evaluation', 'cmb2' ),
         'desc'    => __( 'Link for evaluation', 'cmb2' ),
         'id'      => $prefix . 'evaluation_url',
