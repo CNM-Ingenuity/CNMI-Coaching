f801ec46748c0bd27e9d17ad66eeff33a8e05411
MattHarr1s <mateodelay@gmail.com>
2019-02-04 13:37:26 -0700

    Changed: Added boxes to Certified Coach Trainer Dashboard Task: https://app.asana.com/0/889318959655008/939102540792225

--- wp-content/themes/cnmi-coaching-theme/partials/dashboard/certified-coach-trainer-dashboard.php
+++ wp-content/themes/cnmi-coaching-theme/partials/dashboard/certified-coach-trainer-dashboard.php
@@ -1,4 +1,31 @@
 <?php
+get_template_part('partials/elements/top-matter');
 ?>
 
-<h2>You are a Certified Coach Trainer!</h2>
+<div class='dashboard-block-container'>
+
+	<a href='/my-certifications'>
+		<div class='one-half first my-certifications'>
+			<h3>My Organizations Trainings</h3>
+		</div>
+	</a>
+
+	<a href='/my-trainings'>
+		<div class='one-half my-trainings'>
+			<h3>My organizations License</h3>
+		</div>
+	</a>
+
+	<a href='/my-resources'>
+		<div class='one-half first my-resources'>
+			<h3>Resources</h3>
+		</div>
+	</a>
+
+	<a href='/my-trainings'>
+		<div class='one-half my-trainings'>
+			<h3>My organizations License</h3>
+		</div>
+	</a>
+
+</div>
