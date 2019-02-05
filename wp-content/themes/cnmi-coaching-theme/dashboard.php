7e6c40d925c741aa9296da7047b82df07c5cddd6
MattHarr1s <mateodelay@gmail.com>
2019-02-05 10:13:27 -0700

    Changed: Removed commented out echos Task: https://app.asana.com/0/889318959655008/939102540792225

--- wp-content/themes/cnmi-coaching-theme/dashboard.php
+++ wp-content/themes/cnmi-coaching-theme/dashboard.php
@@ -12,23 +12,18 @@ function show_appropriate_dashboard_to_user() {
 
 		$plan_id = $memberships[0]->{"plan_id"};
 		if ($plan_id == 406) {
-			// echo '<h2>You are a Certified Coach!</h2>';
 			get_template_part('partials/dashboard/certified-coach-dashboard');
 		} elseif ($plan_id == 407) {
-			// echo '<h2>You are a Coach in Training!</h2>';
 			get_template_part('partials/dashboard/coach-in-training-dashboard');
 		} elseif ($plan_id == 408) {
-			// echo '<h2>You are a Contracting Organization!</h2>';
 			get_template_part('partials/dashboard/contracting-organization-dashboard');
 		} elseif ($plan_id == 410) {
-			// echo '<h2>You are a Licensed Organization!</h2>';
 			get_template_part('partials/dashboard/licensed-organization-dashboard');
-		} elseif ($plan_id == 411) {
-			// echo '<h2>You are a Certified Coach Trainer!</h2>';
+		} elseif ($plan_id == 411) {			
 			get_template_part('partials/dashboard/certified-coach-trainer-dashboard');
 
 		}
-	}	
+	}
 }
 add_action('genesis_entry_content', 'show_appropriate_dashboard_to_user');
 
