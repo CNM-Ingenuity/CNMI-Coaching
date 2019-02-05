270a0995e0b488d2831fd1a3d7a099492b542a22
MattHarr1s <mateodelay@gmail.com>
2019-02-05 14:56:41 -0700

    Added: conditon for my-resources template for certified coach trainer in cerfication-list.php Task: https://app.asana.com/0/889318959655008/939102540792225

--- wp-content/themes/cnmi-coaching-theme/partials/elements/certification-list.php
+++ wp-content/themes/cnmi-coaching-theme/partials/elements/certification-list.php
@@ -6,17 +6,16 @@ if($memberships){
 	if ($plan_id == 407) {
 		// certified coach in training
 		include(locate_template('partials/elements/certification-lists/certified-coach-in-training.php'));
-	} elseif ($plan_id == 411) {		
-		// certified coach trainer	
+	} elseif ($plan_id == 411) {
+		// certified coach trainer
 		// we want trainings listed or unique certifications based on the page
 		$template = get_page_template_slug();
 		if($template === 'my-trainings.php') {
 			include(locate_template('partials/elements/certification-lists/certified-coach-trainer.php'));
 		} else if ($template === 'my-certifications.php') {
 			include(locate_template('partials/elements/certification-lists/unique-certifications-coach-trainer.php'));
+		} else if ($template === 'my-resources.php') {
+			include(locate_template('partials/elements/certification-lists/unique-certifications-coach-trainer.php'));
 		}
 	}
 }
-
-
-
