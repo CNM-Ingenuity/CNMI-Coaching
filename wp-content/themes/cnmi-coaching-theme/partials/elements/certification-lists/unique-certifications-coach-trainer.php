e428c67a153f41ff646fd48c78cae1eef6fb0104
MattHarr1s <mateodelay@gmail.com>
2019-02-07 11:33:19 -0700

    Added: item-padding class to unique certifications Task: https://app.asana.com/0/889318959655008/939102540792225

--- wp-content/themes/cnmi-coaching-theme/partials/elements/certification-lists/unique-certifications-coach-trainer.php
+++ wp-content/themes/cnmi-coaching-theme/partials/elements/certification-lists/unique-certifications-coach-trainer.php
@@ -1,9 +1,10 @@
 <?php
+//* Add custom body class to the head
 $certifications = CNMI_Certifications::get_unique_certifications_by_coach_id($user_id);
 foreach ($certifications as $certificationId => $name) {
 	?>
 		<a href='<?php echo $linkAddress . $certificationId; ?>'>
-			<div class="item">
+			<div class="item item-padding">
 				<h3 class="title"><?php echo $name; ?></h3>
 			</div>
 		</a>
