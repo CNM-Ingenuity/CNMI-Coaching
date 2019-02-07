e05deb063f93cbfb24c81d5d98dd4e0a030eaf8e
MattHarr1s <mateodelay@gmail.com>
2019-02-07 11:14:10 -0700

    Added: markup to coach-trainer single. Task: https://app.asana.com/0/889318959655008/939102540792225

--- wp-content/themes/cnmi-coaching-theme/partials/certification-singles/coach-trainer.php
+++ wp-content/themes/cnmi-coaching-theme/partials/certification-singles/coach-trainer.php
@@ -5,11 +5,32 @@ if($post) {
 	$eventType = $post->post_title;
 }
 $breadcrumbs = [
-	"Resources" => "/my-certifications",
+	"My Certifications" => "/my-certifications",
 	$eventType => "#",
 ];
 include(locate_template('partials/elements/breadcrumbs.php'));
 include(locate_template('partials/elements/top-matter.php'));
 if($post) {
-    echo wpautop($post->post_content);
+	?>
+	<div class="description">
+		<?php
+    	echo wpautop($post->post_content);
 }
+?>
+	  <!-- The buttons below might not be necessary  -->
+		<!-- <div class="certification-buttons">
+			<div class="one-half first">
+				<a class="button item-button" href="/transcript">
+					<p>View Transcript</p>
+					<img src="/wp-content/uploads/2019/01/download-arrow.png">
+				</a>
+			</div>
+			<div class="one-half">
+				<a class="button item-button" href="/training/record">
+					<p>Take Assessment</p>
+					<span class="dashicons dashicons-media-document"></span>
+				</a>
+			</div>
+		</div> -->
+
+	</div>
