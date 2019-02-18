bdeeac52d6ebb467075287cfd60fb0be7a36f804
Eric Debelak <ericdebelak@gmail.com>
2019-02-18 10:01:32 -0700

    ADDED: only show edit events to licensing orgs and only pull in their coaches

--- wp-content/themes/cnmi-coaching-theme/tribe-events/community/modules/choose-coaches.php
+++ wp-content/themes/cnmi-coaching-theme/tribe-events/community/modules/choose-coaches.php
@@ -14,7 +14,7 @@ defined( 'WPINC' ) or die;
  *
  */
 
-$users = get_users(  );
+$users = get_users( array('meta_key' => 'licensing_org', 'meta_value' => get_current_user_id()) );
 
 $user_options = array();
 
