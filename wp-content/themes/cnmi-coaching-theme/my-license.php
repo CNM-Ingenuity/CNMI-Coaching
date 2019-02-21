0e892dc70b62991f5d229e42bbc37dc6ac37549b
Eric Debelak <ericdebelak@gmail.com>
2019-02-21 11:35:31 -0700

    ADDED: renewal and docs to license page

--- wp-content/themes/cnmi-coaching-theme/my-license.php
+++ wp-content/themes/cnmi-coaching-theme/my-license.php
@@ -4,6 +4,15 @@
  */
 remove_action( 'genesis_entry_header', 'genesis_do_post_title' );
 
+//* Add custom body class to the head
+add_filter( 'body_class', 'form_body_class' );
+function form_body_class( $classes ) {
+	
+	$classes[] = 'progress-form-page';
+	return $classes;
+	
+}
+
 function show_my_resources() {
 	$breadcrumbs = [
 		"My Organization's License" => "#"
@@ -11,7 +20,13 @@ function show_my_resources() {
 	include(locate_template('partials/elements/breadcrumbs.php'));
 	get_template_part('partials/elements/top-matter');
 	$coaches = CNMI_Licensing_Org::get_coaches();
+	$current_user_id = get_current_user_id();
+	$renewal_date = get_user_meta( $current_user_id, 'licensing_renewal_date', true );
+	$licensing_document = get_user_meta( $current_user_id, 'licensing_document', true );
 	?>
+	<div class="item">
+		<p class="date">Renewal Date: <?php echo $renewal_date; ?></p>
+	</div>
 	<table>
 		<tr>
 			<th>Trainer Name</th>
@@ -28,6 +43,7 @@ function show_my_resources() {
 	}
 	?>
 	</table>
+	<a class="button item-button" target="_blank" href="<?php echo $licensing_document;?>"><p>License Document</p><span class="dashicons dashicons-media-text"></span></a>
 	<?php
 }
 add_action('genesis_entry_content', 'show_my_resources');
