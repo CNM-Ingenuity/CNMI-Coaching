7330d953312183c983db3e189147aecdce87e419
Asya Nikitina <a.f.nikitina@gmail.com>
2019-02-21 21:26:54 -0700

    UPDATED: rewrote tests for Track Coaching Hours form (cnmi-coaching)

--- cypress/e2e/track-coaching-hours.js
+++ cypress/e2e/track-coaching-hours.js
@@ -1,33 +1,44 @@
 describe('Track Coaching Hours', () => {
-    // test the form itself including the submit button
-    it('can track coaching hours', () => {
+    beforeEach(() => {
+        cy.visit('/track-coaching-hours/?certification=1')
+     })
+
+    //each input field accepts input
+    it('accepts input for name', () => { 
         const client = 'Jane Doe'
-        const sessionDate = '2019-02-10'
-        const minutes = '45'
-        const comments = 'love this lesson'
 
-        // first a user needs to be logged in
-
-
-
-        cy
-            .visit('/track-coaching-hours/?certification=1')
-            .get('[label="client_name"]')
+        cy.get('[label="client_name"]')
             .click()
             .type(client)
-            .get('[label="date"]')
+            .should('have.value', client)
+    })
+
+    it('accepts input for date', () => {
+        const sessionDate = '2019-02-10'
+
+        cy.get('[label="date"]')
             .click()
             .type(sessionDate)
-            .get('[label="minutes"]')
+            .should('have.value', sessionDate)
+    })
+
+    it('accepts input for minutes', () => {
+        const minutes = '45'
+
+        cy.get('[label="minutes"]')
             .click()
             .type(minutes)
-            .get('textarea')
+            .should('have.value', minutes)
+    })
+
+    it('accepts input for comments', () => {
+        const comments = 'love this lesson'
+
+        cy.get('textarea')
             .click()
             .type(comments)
-            .get('[type="submit"]')
-            .click()
-            .url()
-            .should('eq', `${Cypress.config().baseUrl}/track-coaching-hours/?certification=1`)
-    })  
+            .should('have.value', comments)
+    })
+
 
 })
