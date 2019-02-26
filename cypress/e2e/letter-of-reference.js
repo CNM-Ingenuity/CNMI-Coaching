111136d75dfcb8ab206f630c161f2742fc9afae8
Asya Nikitina <a.f.nikitina@gmail.com>
2019-02-26 16:08:09 -0700

    UPDATED: changed the path to the json file with login credentials (cnmi)

--- cypress/e2e/letter-of-reference.js
+++ cypress/e2e/letter-of-reference.js
@@ -1,9 +1,12 @@
 describe('Letter of Reference', () => {
     function login() {
-        cy.visit('/login/')
-        cy.get('#user_login').type('matt+cit@11online.us')
-        cy.get('#user_pass').type('pSc3gM0IpbicjGwarXC2NyfP')
-        cy.get('#wp-submit').click()
+        cy.fixture('users/admin-cit')
+            .then((admin) => {
+                cy.visit('/login/')
+                cy.get('#user_login').type(admin.email)
+                cy.get('#user_pass').type(admin.password)
+                cy.get('#wp-submit').click()
+            })
     }
 
     before(() => {
@@ -14,6 +17,29 @@ describe('Letter of Reference', () => {
         cy.visit('/submit-letters-of-reference/?certification=1')
     })
 
+    it(`displays 'Submit Letters of Reference'`, () => {
+        cy.get('.first > h3')
+            .invoke('text')
+            .then((text) => {
+                expect(text.trim()).to.contain('Submit Letters of Reference')
+            })
+    })
+
+    it(`has 'In Training' sign`, () => {
+        cy.get('.user-name > p')
+            .invoke('text')
+            .then((text) => {
+                expect(text.trim()).to.contain('In Training')
+            })
+    })
+
+    it(`redirects to dashboard upon clickin on gears icon`, () => {
+        cy.get('.user-name > p > a > .dashicons')
+            .click()
+        cy.url()
+            .should('eq', `${Cypress.config().baseUrl}/dashboard/`)
+    })
+
     it('fails to submit a letter of reference since no file was selected', () => {
         cy.get('div > input[type=file]')
             .click()
@@ -21,5 +47,6 @@ describe('Letter of Reference', () => {
         cy.get('[type="submit"]')
             .click()
         cy.getByText(/^Some information is missing*/i)
+            .should('be.visible')
     })
 })
