3bfe4163f20f9e9944c974cfeab6df44fe332f6a
Asya Nikitina <a.f.nikitina@gmail.com>
2019-02-26 13:53:44 -0700

    CREATED: created tests for 'My Certifications' page (cnmi)

---
+++ cypress/e2e/my-certifications.js
@@ -0,0 +1,62 @@
+describe('My Certifications', () => {
+    function login() {
+        cy.visit('/login/')
+        cy.get('#user_login').type('matt+cit@11online.us')
+        cy.get('#user_pass').type('pSc3gM0IpbicjGwarXC2NyfP')
+        cy.get('#wp-submit').click()
+    }
+
+    before(() => {
+        login()
+    })
+
+    beforeEach(() => {
+        cy.visit('/my-certifications/')
+    })
+
+    it(`has 'In Training' sign`, () => {
+        cy.get('.user-name > p')
+            .invoke('text')
+            .then((text) => { 
+                expect(text.trim()).to.contain('In Training')
+            })
+    })
+
+    it(`redirects to dashboard upon clickin on gears icon`, () => {
+        cy.get('a[href="/dashboard"] > span')
+            .click()
+        cy.url()
+            .should('eq', `${Cypress.config().baseUrl}/dashboard/`)
+    })
+
+    it(`each 'Academic Coach Training' box should refer to the corresponding page`, () => {
+        cy.get('a[href^="/my-certification?"]')
+            .should('have.attr', 'href')
+            .then(($href) => { 
+                cy.visit(`${Cypress.config().baseUrl}${$href}`)
+                cy.get('.entry-content > div.top-matter.wrap h3')
+                    .invoke('text')
+                    .then((text) => {
+                        expect(text.trim()).to.contain('Academic Coach Training')
+                    })
+            })
+    })
+
+    it(`each 'Academic Coach Training' box upon clicking anywhere on it redirects to the corresponding page`, () => {
+        cy.get('a[href^="/my-certification?"]')
+            .should('have.attr', 'href')
+            .invoke('toString')
+            .then((text) => {
+                cy.get(`a[href="${text}"] > .item`)
+                    .click({ multiple: true, force: true })
+
+                cy.url()
+                    .invoke('toString')
+                    .then((text2) => {
+                        const words = text.split('?');
+                        expect(text2).to.be.equal(`${Cypress.config().baseUrl}${words[0]}/?${words[1]}`)
+                    })
+            })    
+    })
+
+})
