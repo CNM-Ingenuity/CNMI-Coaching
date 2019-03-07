e0a88bb6f22a542c22c29152114c6d37902b1a3e
Asya Nikitina <a.f.nikitina@gmail.com>
2019-03-07 15:45:30 -0700

    UPDATED: added more tests for the 'Submit Event' view (cnmi)

--- cypress/e2e/schedule-training.js
+++ cypress/e2e/schedule-training.js
@@ -77,6 +77,24 @@ describe('Submit an Event', () => {
             .should('have.value', currText)
     }
 
+    function testText(selector, textContent, tag) {
+        cy.get(selector)
+            .contains(textContent)
+            .should('be.visible')
+            .should('have.prop', 'tagName')
+            .and('eq', tag)           
+    }
+
+    function testSpecialElement(selector, isVisible, arr) {
+        cy.get(selector)
+
+        isVisible ? cy.should('be.visible') : cy.should('not.be.visible')
+
+        for (let i = 0; i < arr.length; i++) {
+            (i === arr.length - 1) ? cy.and('have.attr', arr[i][0], arr[i][1]) : cy.should('have.attr', arr[i][0], arr[i][1])
+        }
+    }
+
 
     before(() => {
         login()
@@ -87,32 +105,19 @@ describe('Submit an Event', () => {
     })
 
     it(`displays 'Submit an Event' main title`, () => {
-        cy.get('h1.entry-title')
-            .invoke('text')
-            .then((text) => {
-                expect(text.trim()).to.contain('Submit an Event')
-            })
+        testText('h1.entry-title', 'Submit an Event', 'H1')
     })
 
     it(`displays 'Schedule a Training' title`, () => {
-        cy.get('.first > h3')
-            .invoke('text')
-            .then((text) => {
-                expect(text.trim()).to.contain('Schedule a Training')
-            })
+        testText('.first > h3', 'Schedule a Training', 'H3')
     })
 
     it(`says 'Matt Harris' next to the gears icon`, () => {
-        cy.get('.user-name > p')
-            .invoke('text')
-            .then((text) => {
-                expect(text.trim()).to.contain('Matt Harris')
-            })
+        testText('.user-name > p', 'Matt Harris', 'P')
     })
 
     it(`redirects to dashboard upon clickin on gears icon`, () => {
         cy.get('.user-name > p > a')
-            .as('myLink')
             .should('have.attr', 'href')
             .then((href) => {
                 cy.visit(href)
@@ -134,12 +139,7 @@ describe('Submit an Event', () => {
     })
 
     it(`has 'EVENT TIME & DATE' section`, () => {
-        cy.get('.tribe-section-header > h3')
-            .should('be.visible')
-            .invoke('text')
-            .then((text) => {
-                expect(text.trim()).to.contain('Event Time & Date')
-            })
+        testText('.tribe-section-header > h3', 'Event Time & Date', 'H3')
     })
 
     it(`has 'Start/End:' label`, () => {
@@ -170,11 +170,9 @@ describe('Submit an Event', () => {
     })
 
     it(`has 'Event Timezone' select element`, () => {
-        cy.get('#event-timezone')
-            .should('have.attr', 'name', 'EventTimezone')
-            .should('have.attr', 'data-timezone-label', 'Timezone:')
-            .should('have.attr', 'style', 'display: none;')
-            .and('have.attr', 'data-timezone-value')
+        const arr = [['name', 'EventTimezone'], ['data-timezone-label', 'Timezone:'], ['style', 'display: none;']]
+
+        testSpecialElement('#event-timezone', false, arr)
     })
 
     it(`has 'Schedule multiple events' button`, () => {
@@ -194,34 +192,117 @@ describe('Submit an Event', () => {
     })
 
     it(`has 'EVENT IMAGE' title`, () => {
-        cy.getByText(/^Event Image$/i)
-            .should('be.visible')
-            .should('have.prop', 'tagName')
-            .and('eq', 'H3')
+        testText('.tribe-section-image-uploader > .tribe-section-header > h3', 'Event Image', 'H3')
     })
 
-    it.only(`has image upload area`, () => {
-        cy.get('.note > p')
-            .should('be.visible')
-            .invoke('text')
-            .then((text) => {
-                expect(text.trim()).to.contain('Choose a .jpg, .png, or .gif file under 50 MB in size.')
-            })
+    it(`has an image upload area`, () => {
+        const arr = [['type', 'file'], ['name', 'event_image'], ['class', 'EventImage']]
+
+        testText('.note > p', 'Choose a .jpg, .png, or .gif file under 50 MB in size.', 'P')       
+        testLabel('.form-controls > label:nth-of-type(2)', 'EventImage', 'Event Image', false, true)
+        testLabel('.form-controls > label:nth-of-type(3)', 'uploadFile', 'Upload File', false, true)
+        testSpecialElement('#EventImage', true, arr)
     })
 
+    it(`has 'TRAINING TYPE' label and corresponding dropdown item`, () => {
+        const arr1 = [['type', 'text'], ['aria-haspopup', 'true'], ['role', 'button']]
+        const arr2 = [['name', 'tax_input[tribe_events_cat]'], ['placeholder', 'Search from existing categories'], ['data-source', 'search_terms']]
 
+        testText(':nth-child(8) > .tribe-section-header > h3', 'Training Type', 'H3')
+        testText(':nth-child(8) > .tribe-section-header > span', '(required)', 'SPAN')
 
+        cy.get('#s2id_autogen3 > a.select2-choice.select2-default')
+            .should('have.attr', 'href')
+        
+        testText('#select2-chosen-4', 'Search from existing categories', 'SPAN')
+        testSpecialElement('#s2id_autogen4', true, arr1)
+        testSpecialElement('input.tribe-dropdown.tribe-dropdown-created', false, arr2)
+    })
 
+    it(`has 'COACHES FOR THIS EVENT' label and corresponding input field`, () => {
+        const arr = [['type', 'text'], ['autocomplete', 'off'], ['autocorrect', 'off'], ['autocapitalize', 'off'], ['placeholder', '']]
 
+        testText(':nth-child(9) > .tribe-section-header > h3', 'Coaches for this Event', 'H3')
+        testText(':nth-child(9) > .tribe-section-header > span', '(required)', 'SPAN')
+        testSpecialElement('#s2id_autogen1', true, arr)
+    })
 
-    
+    it(`has 'VENUE DETAILS' label and corresponding input field`, () => {
+        const arr = [['type', 'text'], ['aria-haspopup', 'true'], ['role', 'button']]
 
+        testText('#event_tribe_venue > .tribe-section-header > h3', 'Venue Details', 'H3')
+        testText('.saved-venue-table-cell > label', 'Venue:', 'LABEL')
+        testSpecialElement('.select2-container.tribe-dropdown > input.select2-focusser.select2-offscreen', true, arr)
 
+        cy.get('#event_tribe_venue .select2-container.linked-post-dropdown > a.select2-choice')
+            .should('be.visible')
+            .should('have.attr', 'href')
+    })
 
+    it(`has 'ORGANIZER DETAILS' label and corresponding items`, () => {
+        const arr = [['type', 'text'], ['aria-haspopup', 'true'], ['role', 'button'], ['aria-labelledby', 'select2-chosen-6']]
+        
+        testText('#event_tribe_organizer > .tribe-section-header > h3', 'Organizer Details', 'H3')
+        testText('.saved-organizer-table-cell > label', 'Organizer:', 'LABEL')
+        testSpecialElement('#s2id_autogen6', true, arr)
 
+        cy.get('#event_tribe_organizer .select2-container.tribe-dropdown > a.select2-choice')
+            .should('be.visible')
+            .should('have.attr', 'href')
+        
+        testText('#select2-chosen-6', 'Create or Find an Organizer', 'SPAN')
 
+        cy.get('#event_tribe_organizer a.dashicons.dashicons-trash.tribe-delete-this')
+            .as('myLink')
+            .should('be.visible')
+            .and('have.attr', 'href', '#')
+        
+        cy.get('@myLink')
+            .find('span')
+            .should('have.class', 'screen-reader-text')
+            .and('have.text', 'Delete this')
 
+        cy.get('#event_tribe_organizer a.tribe-add-post.tribe-button.tribe-button-secondary')
+            .should('be.visible')
+            .should('have.attr', 'href', '#')
+            .and('have.text', 'Add another organizer')
 
+    })
 
+    it(`has 'EVENT WEBSITE' label and corresponding items`, () => {
+        const arr = [['type', 'text'], ['name', 'EventURL'], ['placeholder', 'Enter URL for event information'], ['size', '25']]
 
+        testText('.tribe-section-website > .tribe-section-header > h3', 'Event Website', 'H3')
+        testText('tr.tribe-section-content-row > td.tribe-section-content-label > label[for="EventURL"]', 'External Link:', 'LABEL')
+        testSpecialElement('#EventURL', true, arr)
+    })
+
+    it(`has 'EVENT COST' label and corresponding items`, () => {
+        const arr = [['type', 'text'], ['name', 'EventCost'], ['class', 'cost-input-field'], ['size', '6']]
+
+        testText('.tribe-section-cost > .tribe-section-header > h3', 'Event Cost', 'H3')
+        testText('tr.tribe-section-content-row > td.tribe-section-content-label > label[for="EventCost"]', 'Cost:', 'LABEL')
+        testSpecialElement('#EventCost', true, arr)
+        testText('.tribe-section-content-field > p', 'Leave blank to hide the field. Enter a 0 for events that are free.', 'P')
+    })
+
+    function fillInRequired(selectors, values) {
+        const len = selectors.length
+
+        for (let i = 0; i < len; i++) {
+            cy.get(selectors[i])
+                .clear()
+                .type(values[i])
+                .should('have.value', values[i])
+        }
+    }
+    /*
+    it.only(`has 'Add New Event' label and corresponding items`, () => {
+        const selectors = ['#post_title', '#post_content']
+        const values = ['React Meetup', 'Discussions about ReactJS']
+
+        fillInRequired(selectors, values)
+
+     })
+     */
 })
