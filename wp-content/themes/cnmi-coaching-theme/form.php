f6aed1261e99a9c57d62a9591ae25c9b303c5ea2
MattHarr1s <mateodelay@gmail.com>
2019-03-07 19:22:23 -0700

    ADDED: Form script to template, basic styles for form. TASK: https://app.asana.com/0/search/1113252253949316/1112504074057615

--- wp-content/themes/cnmi-coaching-theme/form.php
+++ wp-content/themes/cnmi-coaching-theme/form.php
@@ -3,12 +3,19 @@
  * Template Name: Form
  */
 //* Add custom body class to the head
-add_filter( 'body_class', 'login_body_class' );
-function login_body_class( $classes ) {
+add_filter( 'body_class', 'form_body_class' );
+function form_body_class( $classes ) {
 
-	$classes[] = 'form-page';
+	$classes[] = 'form-page fast-form';
 	return $classes;
 
+}
+
+add_action('genesis_entry_content', 'add_contact_form_to_page');
+function add_contact_form_to_page(){
+	?>
+	<script type="text/javascript" id="jsFastForms" src="https://sfapi-sandbox.formstack.io/FormEngine/Scripts/Main.js?d=KVHvMtU34D97ATrNrJAyTv4htltq8KpsxMIR6HmIg4eK3K0Yb0ewhMi0s1M2VQBB"></script>
+	<?php
 }
 
 genesis();
