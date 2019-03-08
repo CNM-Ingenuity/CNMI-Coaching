14305f745026ac68729d38ebfabf758d8ba4ae0a
Asya Nikitina <a.f.nikitina@gmail.com>
2019-03-08 11:30:44 -0700

    UPDATED: refactored/added more tests to the end user agreement (cnmi)

--- cypress/e2e/end-user-agreement.js
+++ cypress/e2e/end-user-agreement.js
@@ -9,6 +9,14 @@ describe('Coach End User Agreement', () => {
             })
     }
 
+    function testText(selector, textContent, tag) {
+        cy.get(selector)
+            .contains(textContent)
+            .should('be.visible')
+            .should('have.prop', 'tagName')
+            .and('eq', tag)
+    }
+
     before(() => {
         login()
     })
@@ -18,19 +26,11 @@ describe('Coach End User Agreement', () => {
     })
 
     it(`displays 'Coach End User Agreement'`, () => {
-        cy.get('.first > h3')
-            .invoke('text')
-            .then((text) => {
-                expect(text.trim()).to.contain('Coach End User Agreement')
-            })
+        testText('.first > h3', 'Coach End User Agreement', 'H3')
     })
 
-    it(`has 'In Training' sign`, () => {
-        cy.get('.user-name > p')
-            .invoke('text')
-            .then((text) => {
-                expect(text.trim()).to.contain('In Training')
-            })
+    it(`has 'In Training' sign next to the gears icon`, () => {
+        testText('.user-name > p', 'In Training', 'P')
     })
 
     it(`redirects to dashboard upon clickin on gears icon`, () => {
@@ -40,12 +40,32 @@ describe('Coach End User Agreement', () => {
             .should('eq', `${Cypress.config().baseUrl}/dashboard/`)
     })
 
+    it(`has 'Select file to upload:' label`, () => {
+        testText('#agreement-upload-form > div', 'Select file to upload:', 'DIV')
+    })
+
+    it(`has an input for file upload`, () => {
+        cy.get('#agreement-upload-form > div input[type="file"]')
+            .should('be.visible')
+            .should('have.attr', 'name', 'file')
+            .and('have.attr', 'type', 'file')
+    })
+
+    it(`has a submit button for file upload`, () => {
+        cy.get('input[type="submit"]')
+            .should('be.visible')
+            .should('have.value', 'Upload File')
+            .should('have.attr', 'name', 'submit')
+            .and('have.attr', 'required')
+    })
+
     it('fails to upload since no file was selected', () => {
         cy.get('div > input[type=file]')
             .click()
-
+        
         cy.get('[type="submit"]')
             .click()
+        
         cy.getByText(/^Some information is missing*/i)
             .should('be.visible')
     })   
