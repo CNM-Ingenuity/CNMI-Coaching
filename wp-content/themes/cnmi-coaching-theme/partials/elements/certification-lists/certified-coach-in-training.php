79d3be41d6145da02324fcbf25c9d8dd27fd60e4
MattHarr1s <mateodelay@gmail.com>
2019-04-09 13:39:14 -0600

    ADDED: Conditional for redirecting my certifications to a single certification if there is only one certification. TASK: https://app.asana.com/0/889318959655008/1112504074057605

--- wp-content/themes/cnmi-coaching-theme/partials/elements/certification-lists/certified-coach-in-training.php
+++ wp-content/themes/cnmi-coaching-theme/partials/elements/certification-lists/certified-coach-in-training.php
@@ -4,6 +4,9 @@ foreach ($certifications as $certification) {
 	$eventID = $certification->event_id;
 	$eventType = CNMI_Events::get_event_type($eventID);
 	$eventStartDate = CNMI_Events::get_event_start_date($eventID);
+	if(count($certifications) === 1) {
+			echo "<script> window.location.href='". $linkAddress . $certification->id ."';</script> ";
+		}
 	if($eventStartDate) {
 		$eventStartDate = $eventStartDate->format('m/d/Y');
 	}
