462b73c7a53a2e2a156237c4ebc56bbed06ac4ea
MattHarr1s <mateodelay@gmail.com>
2019-07-25 17:26:59 -0600

    ADDED: function to restrict events cal events for licensed orgs.

--- wp-content/themes/cnmi-coaching-theme/functions.php
+++ wp-content/themes/cnmi-coaching-theme/functions.php
@@ -337,6 +337,7 @@ function training_calendar_grid(){
 	$output .= "<h1>Training Calendar</h1>";
 	$count = 0;
 	foreach($events as $event) {
+		var_dump($event);
 		$eventStartTime = new DateTime($event->EventStartDate, $tz);
 		$eventEndTime = new DateTime($event->EventEndDate, $tz);
 		if($count % 2 === 0) {
@@ -756,4 +757,26 @@ function tribe_tickets_fix_etp_attendee_registration_page_for_genesis() {
 }
 function do_post_content() {
 	the_content();
+}
+
+add_action( 'pre_get_posts', 'restrict_license_org_events' );
+
+//Restrict license org events to events created by that org. 
+function restrict_license_org_events( $query ) {
+	 $post_type = $query->get('post_type');
+	if(! is_admin() && $post_type == 'tribe_events'){
+		$current_user = wp_get_current_user();
+	  $user_id = $current_user->ID;
+		$memberships = wc_memberships_get_user_active_memberships( $user_id );
+		$plan_id = $memberships[0]->{"plan_id"};
+		if($plan_id == 410){
+	 		$query->set('meta_query', array(
+				array('key' => '_cnmi_event_metabox_licensing_org_multicheckbox',
+							'value' => $user_id,
+							'compare' => 'like'
+						)
+					)
+			);
+		}
+	 }
 }
